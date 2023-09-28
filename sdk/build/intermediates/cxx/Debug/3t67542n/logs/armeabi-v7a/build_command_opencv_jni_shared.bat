c7db280e67d1d19ff8a821239971c9dc2e85c170
bgu303 <bgu303@myy.haaga-helia.fi>
2023-09-28 13:29:19 +0300

    fixing errors

---
+++ sdk/build/intermediates/cxx/Debug/3t67542n/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Jukka\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Ohjelmia\\solita-warehouse\\sdk\\.cxx\\Debug\\3t67542n\\armeabi-v7a" ^
+  opencv_jni_shared
