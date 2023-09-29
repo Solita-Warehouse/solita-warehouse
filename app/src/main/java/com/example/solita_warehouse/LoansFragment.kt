5132041aede105c87e9d3437d3ce9e70cc1a8f5a
Pasi Mikkola <pasimikkola111@gmail.com>
2023-09-29 14:11:56 +0300

    RecyclerView

--- app/src/main/java/com/example/solita_warehouse/LoansFragment.kt
+++ app/src/main/java/com/example/solita_warehouse/LoansFragment.kt
@@ -1,38 +1,45 @@
 package com.example.solitawarehouse
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.solita_warehouse.R
+import com.example.solita_warehouse.adapters.ItemAdapter
 import model.Item
 
 class LoansFragment : Fragment() {
 
-    lateinit var itemList : ListView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_loans, container, false)
-        itemList = rootView.findViewById(R.id.itemList)
-        var items = arrayOf(
-            Item("Hammer", "18.09.2023"),
-            Item("Saw", "12.10.2023"),
-            Item("Jackhammer", "02.12.2023"),
-            Item("Screwdriver", "24.12.2023"),
-            Item("Table saw", "18.09.2023"),
-            Item(" Measuring tape", "18.06.2023")
 
-        )
+        val itemList = ArrayList<Item>()
+        val item1 = Item("Hammer", "18.09.2023")
+        val item2 = Item("Saw", "12.10.2023")
 
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, items)
+        itemList.add(item1)
+        itemList.add(item2)
 
-        itemList.adapter = adapter
+        Log.i("Lista", itemList.toString())
+
+        val rvItems = rootView.findViewById<View>(R.id.rvItems) as RecyclerView
+        val adapter = ItemAdapter(itemList)
+
+        rvItems.adapter = adapter
+
+        rvItems.layoutManager = LinearLayoutManager(requireContext())
+
 
         return rootView
     }
---
+++ app/src/main/java/com/example/solita_warehouse/adapters/ItemAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.solita_warehouse.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import com.example.solita_warehouse.R
+import model.Item
+
+
+class ItemAdapter (private val mItems: List<Item>) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // Your holder should contain and initialize a member variable
+        // for any view that will be set as you render a row
+        val nameTextItem = itemView.findViewById<TextView>(R.id.itemName)
+        val returnTextDate = itemView.findViewById<TextView>(R.id.returnDate)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+
+        val itemView = inflater.inflate(R.layout.item_list, parent, false)
+
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(viewHolder: ItemAdapter.ViewHolder, position: Int) {
+        val item: Item = mItems.get(position)
+        val nameTextView = viewHolder.nameTextItem
+        nameTextView.setText(item.name)
+        val returnDateTextView = viewHolder.returnTextDate
+        returnDateTextView.setText(item.returnDate)
+    }
+
+    override fun getItemCount(): Int {
+        return mItems.size
+    }
+}
