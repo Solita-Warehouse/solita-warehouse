021573c150875cf578d7f10207b19b69a2f25636
Elias <eliashelin98@gmail.com>
2023-11-16 15:23:15 +0200

    started on creating rental orders

--- app/src/main/java/data/RentedItemsConnection.kt
+++ app/src/main/java/data/RentedItemsConnection.kt
@@ -6,10 +6,12 @@ import kotlinx.coroutines.withContext
 import model.EnvVariableLoader
 import model.Item
 import model.RentedItem
+import model.User
 import org.apache.xmlrpc.client.XmlRpcClient
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl
 import java.io.EOFException
 import java.net.URL
+
 
 class RentedItemsConnection() {
     private val client = XmlRpcClient()
@@ -21,6 +23,7 @@ class RentedItemsConnection() {
     val PASSWORD_LOCAL = EnvVariableLoader.PASSWORD_LOCAL
     val URL = EnvVariableLoader.URL
     val LOCAL_URL = EnvVariableLoader.URL_LOCAL
+
 
     init {
         modelConfig.serverURL = URL("${URL}/xmlrpc/2/object")
@@ -40,7 +43,7 @@ class RentedItemsConnection() {
             ) as Array<*>
 
             for(data in rentalOrderSearch) {
-                val rentedItem = RentedItem(0, 0, "", "", "", "")
+                val rentedItem = RentedItem(0, 0, "", "", "")
                 for (item in data as Map<*, *>) {
                     if (item.key == "id") {
                         rentedItem.id = item.value as Int
@@ -73,18 +76,60 @@ class RentedItemsConnection() {
     }
 
     suspend fun returnRentedItemsById(): MutableList<RentedItem> = withContext(Dispatchers.IO) {
-        rentedItemsById.clear()
-        Log.i("odoo", rentedItemsById.toString())
-
         val rentedItems = returnRentedItems()
 
         for (item in rentedItems) {
-            if (item.partnerId == currentUser.partnerId) {
+            if (item.partnerId == 7) {
                 rentedItemsById.add(item)
             }
         }
 
+        for (item in rentedItemsById) {
+            Log.i("odoo", item.toString())
+        }
+        Log.i("odoo", rentedItemsById.toString())
         return@withContext rentedItemsById
+    }
+
+    suspend fun rentItem(): User = withContext(Dispatchers.IO) {
+
+        try {
+
+            val orderValues = mapOf(
+                "partner_id" to currentUser.partnerId,
+                "type_id" to 2
+            )
+            // Create the sales order
+            val createResult = client.execute(
+                modelConfig,
+                "execute_kw",
+                listOf(
+                    DB, 2, PASSWORD,
+                    "sale.order", "create",
+                    listOf(listOf(orderValues))
+                )) as Array<Any>
+
+            val orderId = createResult[0] as Int
+
+            // Confirm the sales order (replace with the correct method name)
+            val confirmResult = client.execute(
+                modelConfig,
+                "execute_kw",
+                listOf(
+                    DB, 2, PASSWORD,
+                    "sale.order", "action_confirm",  // Replace with the correct method name
+                    listOf(listOf(orderId))
+                )) as Boolean  // Assuming it returns a boolean indicating success
+
+            if (confirmResult) {
+                Log.i("odoo", "Sales order successfully confirmed with ID: $orderId")
+            } else {
+                Log.i("odoo", "Failed to confirm the sales order.")
+            }
+        } catch (e: Exception) {
+            Log.i("odoo", "Error creating or confirming the sales order: ${e.message}")
+        }
+        return@withContext currentUser
     }
 
 }
