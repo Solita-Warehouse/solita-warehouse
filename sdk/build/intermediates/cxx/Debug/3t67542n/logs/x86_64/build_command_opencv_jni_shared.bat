05152a4a5416759546c1f6d45fce368903f28df6
bgu303 <bgu303@myy.haaga-helia.fi>
2023-11-17 16:48:09 +0200

    Item renting first version.

--- sdk/build/intermediates/cxx/Debug/3t67542n/logs/x86_64/build_command_opencv_jni_shared.bat
+++ sdk/build/intermediates/cxx/Debug/3t67542n/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
-"C:\\Users\\teemu\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+"C:\\Users\\Jukka\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "C:\\Users\\teemu\\softala-app\\solita-warehouse\\sdk\\.cxx\\Debug\\3t67542n\\x86_64" ^
+  "C:\\Ohjelmia\\solita-warehouse\\sdk\\.cxx\\Debug\\3t67542n\\x86_64" ^
   opencv_jni_shared
