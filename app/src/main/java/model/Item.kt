715313fe08a2be86989fdec52b3f82cc4a207b6f
Elias <eliashelin98@gmail.com>
2023-11-28 17:40:21 +0200

    updated item.kt to include description

--- app/src/main/java/model/Item.kt
+++ app/src/main/java/model/Item.kt
@@ -1,21 +1,22 @@
 package model
 
-class Item(val name: String, val id: Int, var quantity: Double) {
+class Item(val name: String, val id: Int, var quantity: Double, val description: String) {
     companion object {
         fun createFromApiData(apiData: Map<*, *>): Item? {
             val name = apiData["name"]?.toString() ?: return null
             val id = apiData["id"] as? Int ?: return null
             val rental = apiData["rental"] as? Boolean ?: return null
             val quantity = (apiData["virtual_available"] as? Double) ?: 0.0
+            val description = apiData["description"]?.toString() ?: return null
 
             if (rental) {
-                return Item(name, id, quantity)
+                return Item(name, id, quantity, description)
             }
 
             return null
         }
     }
     override fun toString(): String {
-        return "Item name: $name - Item id: $id  - Item quantity: $quantity"
+        return "Item name: $name - Item id: $id  - Item quantity: $quantity - Description: $description"
     }
 }
